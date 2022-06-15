0231e82505eb4cb1c27268b2852ff223259f33e6
Fidel <59423@bears.smcc.edu>
2022-06-15 19:13:31 -0400

    added new test

--- Backend/src/app/api/HealthCheck/__tests__/health.test.ts
+++ Backend/src/app/api/HealthCheck/__tests__/health.test.ts
@@ -1,10 +1,10 @@
 /* eslint-disable no-undef */
-import supertest from 'supertest';
+import request from 'supertest';
 import { HTTPServer } from '../../../server/server';
 import routes from '../../routes';
+import assert from 'assert';
 
 const server = new HTTPServer(8080, routes);
-const { app } = server;
 
 describe('port', () => {
   describe('while running, the port, ', () => {
@@ -17,9 +17,18 @@ describe('port', () => {
 
 describe('healthCheck', () => {
   describe('given route is successful', () => {
-    it('should return a status code of 200', async () => {
-      const { statusCode } = await supertest(app).get('/v1/healthCheck');
-      expect(statusCode).toBe(200);
+    test('should return a status code of 200',function() {
+      request('http://localhost:8080')
+          .get('/v1/healthCheck')
+            .expect(200)
+            .expect('Content-Type', 'application/json')
+            .expect(function (res) {
+              assert(res.body.hasOwnProperty('status'));
+              assert(res.body.hasOwnProperty('message'));
+            })
+          .end(function (err, res){
+            if(err) throw  err;
+          })
     });
   });
 });
