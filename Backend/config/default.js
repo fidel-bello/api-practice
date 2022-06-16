337d67c859e8f26e18ca08d14b798d6e73ccd97d
Fidel <59423@bears.smcc.edu>
2022-06-16 19:04:09 -0400

    update

--- Backend/config/default.js
+++ Backend/config/default.js
@@ -1,5 +1,9 @@
+/* eslint-disable no-multi-assign */
+/* eslint-disable no-unused-vars */
+/* eslint-disable semi */
+/* eslint-disable import/no-extraneous-dependencies */
 require('dotenv').config();
 
 const config = module.exports = {
-    URL: process.env.URL
+  URL: process.env.URL
 }
