d6b1841af7c2d31547bf8f14a34db04eddc62533
Fidel <59423@bears.smcc.edu>
2022-06-21 21:03:55 -0400

    added encryption for password with bcrypt

--- Backend/src/app/api/user/userModel.ts
+++ Backend/src/app/api/user/userModel.ts
@@ -1,8 +1,11 @@
+/* eslint-disable consistent-return */
+/* eslint-disable @typescript-eslint/no-this-alias */
 /* eslint-disable import/no-cycle */
 /* eslint-disable import/no-extraneous-dependencies */
 import mongoose, { SchemaDefinition } from 'mongoose';
 import config from 'config';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import { Model } from '../model/mongoModel';
 import { IUserModel } from './interface/user';
 
@@ -21,6 +24,19 @@ userModel.schema.methods.getToken = function getToken() {
     expiresIn: parseInt(config.get('JWT_EXPIRE'), 10), algorithm: config.get('ALGORITHM')
   });
 };
+
+userModel.schema.pre<IUserModel>('save', function encrypt(next): void {
+  const user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.genSalt(parseInt(config.get('BCRYPT'), 10), (err, salt) => {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, (error, hash) => {
+      if (error) return next(error);
+      user.password = hash;
+      next();
+    });
+  });
+});
 
 userModel.model = mongoose.model<IUserModel>('user', userModel.schema);
 
